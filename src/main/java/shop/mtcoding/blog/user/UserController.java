d09728ec12b7b28ad36e5fa0577ba6156b1cbc61
minmeanmin <wed1604@naver.com>
2024-03-15 14:45:44 +0900

    에러 처리 + 인터셉터

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.blog._core.errors.exception.Exception401;
 
 
 @RequiredArgsConstructor
